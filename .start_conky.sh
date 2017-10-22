c21152b15da026c69144aad54d25cac94167b75f
Adam Papai <wooh@wooh.hu>
2017-10-22 09:32:39 +0900

    Start more conky stuff

--- .start_conky.sh
+++ .start_conky.sh
@@ -4,3 +4,5 @@ cd ~/.conky/
 LC_ALL=en_US.UTF-8 conky -c Gotham/Gotham &
 cd ~/.conky/Conky\ Seamod
 conky -c conky_seamod &
+cd ~/repos/conkySimpleForecast/
+LC_ALL=en_US.UTF-8 conky -c conky_simpleforcast &
