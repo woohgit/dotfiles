4a518c2ac6439ad64159d3277791e42640484695
Adam Papai <wooh@wooh.hu>
2018-07-02 10:45:52 +0900

    New wireless card name

--- .conky/Conky Seamod/seamod_rings.lua
+++ .conky/Conky Seamod/seamod_rings.lua
@@ -145,7 +145,7 @@ gauge = {
  --   caption_fg_colour=0xFFFFFF,    caption_fg_alpha=0.5,
 --},
 {
-    name='downspeedf',           arg='wlp1s0',                     max_value=100,
+    name='downspeedf',           arg='wlp2s0',                     max_value=100,
     x=70,                          y=660,
     graph_radius=54,
     graph_thickness=7,
@@ -166,7 +166,7 @@ gauge = {
     caption_fg_colour=0xFFFFFF,    caption_fg_alpha=0.5,
 },
 {
-    name='upspeedf',           arg='wlp1s0',                     max_value=100,
+    name='upspeedf',           arg='wlp2s0',                     max_value=100,
     x=70,                          y=660,
     graph_radius=42,
     graph_thickness=7,
